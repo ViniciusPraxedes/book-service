cd9dd30efb8b77d8d871d63cd6572a8b8d880639
Vinicius Praxedes <vinicius.praxedes@nexergroup.com>
2024-03-04 10:01:05 +0100

    Removed connection to discovery server

--- src/main/java/com/example/bookservice/BookServiceApplication.java
+++ src/main/java/com/example/bookservice/BookServiceApplication.java
@@ -2,11 +2,9 @@ package com.example.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class BookServiceApplication {
 
     public static void main(String[] args) {
